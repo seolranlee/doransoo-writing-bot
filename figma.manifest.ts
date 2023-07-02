a279059da2d31ab52a4ff4d1b82a4b27f211e894
seolranlee <leesugar90@gmail.com>
2023-07-02 20:55:21 +0900

    manifest파일 수정

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,7 +1,7 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
-  name: "Figma-Plugin-React-Vite",
-  id: "1222852692367737510",
+  name: "Doransoo",
+  id: "1257302656334209022",
   api: "1.0.0",
   main: "plugin.js",
   ui: "index.html",
